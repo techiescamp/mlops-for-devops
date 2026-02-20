00706c7a815055f2680944301aaf845d74a89b10
TechiesCamp <106984297+techiescamp@users.noreply.github.com>
2026-02-21 00:40:32 +0530

    [Add] - Data Preparation Scripts

--- phase-1-local-dev/src/config/paths.py
+++ phase-1-local-dev/src/config/paths.py
@@ -9,14 +9,14 @@ DATASET_DIR = BASE_DIR / "datasets"
 RAW_DATA_PATH = DATASET_DIR / "employee_attrition.csv"
 
 # Data preparation
-DATA_PREP_DIR = DATASET_DIR / "data-preparation"
-INGESTION_PATH = DATA_PREP_DIR / "01_ingestion.csv"
-VALIDATION_PATH = DATA_PREP_DIR / "02_validation.csv"
-EDA_PATH = DATA_PREP_DIR / "03_eda_df.csv"
-CLEANING_PATH = DATA_PREP_DIR / "04_cleaning.csv"
-FEATURED_PATH = DATA_PREP_DIR / "05_feature_engg_df.csv"
-PREPROCESSED_TRAIN_PATH = DATA_PREP_DIR / "06_train_df.csv"
-PREPROCESSED_TEST_PATH = DATA_PREP_DIR / "06_test_df.csv"
+DATA_PREP_DIR = DATASET_DIR / "processed"
+INGESTION_PATH = DATA_PREP_DIR / "raw_ingested.csv"
+VALIDATION_PATH = DATA_PREP_DIR / "validated.csv"
+EDA_PATH = DATA_PREP_DIR / "eda.csv"
+CLEANING_PATH = DATA_PREP_DIR / "cleaned.csv"
+FEATURED_PATH = DATA_PREP_DIR / "featured.csv"
+PREPROCESSED_TRAIN_PATH = DATA_PREP_DIR / "train.csv"
+PREPROCESSED_TEST_PATH = DATA_PREP_DIR / "test.csv"
 
 
 # Model artifacts
