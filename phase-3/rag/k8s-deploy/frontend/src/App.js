02e2d530186f0de45f10b22bace7a0abdfacf3f3
Hashim N <Hashim@Hashims-MacBook-Pro.local>
2026-06-02 18:11:15 +0530

    [Update][Made the ui change in k8s deploy folder as same as the pipeline folder frontend]

--- phase-3/rag/k8s-deploy/frontend/src/App.js
+++ phase-3/rag/k8s-deploy/frontend/src/App.js
@@ -11,11 +11,6 @@ function App() {
           <span className='logo'>DM</span>
           <span>DocuMancer</span>
         </h1>
-        <div className='header-details'>
-          <p><b>Model:</b> gpt-4o-mini</p>
-          <p><b>Embeddings:</b> text-embedding-3-small</p>
-          <p><b>Vector store:</b> FAISS</p>
-        </div>
         <p className='copyright'>Powered by <span>&copy; techiescamp 2025</span></p>
       </header>
       
